db74dcbb76a353b57f6cc5e64d4ecde143125023
zheng1748 <zheng1748@outlook.com>
2026-01-14 17:26:19 +0800

    添加项目文件。

---
+++ Infrastructure/Aksl.Infrastructure/Navigation/Events/OnBuildWorkspaceViewEvent.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Events;
+using System;
+using System.Collections;
+
+namespace Aksl.Infrastructure.Events
+{
+    #region Eventbase
+    public class OnBuildWorkspaceViewEventbase : PubSubEvent<OnBuildWorkspaceViewEventbase>
+    {
+        #region Constructors
+        public OnBuildWorkspaceViewEventbase()
+        {
+        }
+        #endregion
+
+        #region Properties
+        public string Name { get; set; }
+
+        public MenuItem CurrentMenuItem { get; set; }
+        #endregion
+    }
+    #endregion
+
+    #region SideBar Tab
+    public class OnBuildHamburgerMenuSideBarTabWorkspaceViewEvent : OnBuildWorkspaceViewEventbase
+    {
+        #region Constructors
+        public OnBuildHamburgerMenuSideBarTabWorkspaceViewEvent()
+        {
+            Name = typeof(OnBuildHamburgerMenuSideBarTabWorkspaceViewEvent).Name;
+        }
+        #endregion
+    }
+    #endregion
+}
